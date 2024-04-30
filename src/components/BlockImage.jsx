f7de6e1358a4c6c2bc677fe5230bae8160bc79a7
garrettomi <omigarrett@gmail.com>
2024-04-30 16:46:50 +0900

    Feat: Implement gradiant overlay

--- src/components/BlockImage.jsx
+++ src/components/BlockImage.jsx
@@ -1,10 +1,12 @@
 export default function BlockImage({src, alt}) {
     const style = {
-        backgroundImage: `url(${src})`
+        backgroundImage: `url(${src})`,
+        position: 'relative'
     };
 
     return (
         <div style={style} className="bg-cover bg-no-repeat bg-center h-72 w-full">
+            <div className="absolute inset-0 bg-white bg-opacity-25" />
             <span className="sr-only">{alt}</span>
         </div>
     );
