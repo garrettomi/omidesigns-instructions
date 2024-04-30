5cdd45e180524419fa4c1e68b7d460dc2bf638b4
garrettomi <omigarrett@gmail.com>
2024-04-30 14:22:09 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <div>Omi Designs Instructions</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BlockImage.jsx
@@ -0,0 +1,5 @@
+export default function BlockImage(src, alt) {
+    return (
+        <img src={src} alt={alt}></img>
+    );
+};
