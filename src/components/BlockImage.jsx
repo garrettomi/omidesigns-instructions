80bd6f0300a214a678594a58921bf4dab9fd3218
garrettomi <omigarrett@gmail.com>
2024-05-07 20:53:39 +0900

    Fix: Center header title

--- src/components/BlockImage.jsx
+++ src/components/BlockImage.jsx
@@ -8,7 +8,7 @@ export default function BlockImage({src, alt}) {
 
     return (
         <div style={style} className="bg-cover bg-no-repeat bg-center h-72 w-full">
-            <div className="absolute inset-0 flex justify-center items-center z-20">
+            <div className="absolute inset-0 flex justify-center items-center pt-20 z-20">
                 <HeaderTitle text="ムービーの作り方" />
             </div>
             <div className="absolute inset-0 bg-white bg-opacity-25 z-10" />
