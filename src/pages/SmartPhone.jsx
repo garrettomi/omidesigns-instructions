9d2afed40037db64ef7c553e034050ad7c1e05e4
garrettomi <omigarrett@gmail.com>
2024-06-04 20:03:39 +0900

    Feat: Migrate Smart Phone Images

--- src/pages/SmartPhone.jsx
+++ src/pages/SmartPhone.jsx
@@ -1,24 +1,26 @@
 import HashLinkList from "../components/common/HashLinkList";
 
-import firstPhoto from "../assets/共有マーク.png";
-import secondPhoto from "../assets/テキスト-1.png";
-import thirdPhoto from "../assets/テキスト-2.png";
-import fourthPhoto from "../assets/upload.png";
-import fifthPhoto from "../assets/写真の置き換え.png";
-import sixthPhoto from"../assets/前面・背面へ移動-1.png";
-import seventhPhoto from"../assets/前面・背面へ移動-2.png";
-import eighthPhoto from "../assets/音楽の確認方法-1.png";
-import ninthPhoto from "../assets/音楽の確認方法-2.png";
-import tenthPhoto from "../assets/音楽の削除の仕方.png";
-import eleventhPhoto from "../assets/音楽のエフェクト-1.png";
-import twelfthPhoto from "../assets/音楽のエフェクト-2.png";
-import thirteenthPhoto from "../assets/page-1.png";
-import fourteenthPhoto from "../assets/page-2.png";
-import fifteenthPhoto from "../assets/アニメーション-1.png";
-import sixteenthPhoto from "../assets/アニメーション-2.png";
-import seventeenthPhoto from "../assets/透明度-1.png";
-import eighteenthPhoto from "../assets/透明度-2.png";
-import nineteenthPhoto from "../assets/ロック.png";
+import firstPhoto from "../assets/mobile-1.png";
+import secondPhoto from "../assets/mobile-2.png";
+import thirdPhoto from "../assets/mobile-3.png";
+import fourthPhoto from "../assets/mobile-4.png";
+import fifthPhoto from "../assets/mobile-5.png";
+import sixthPhoto from "../assets/mobile-6.png";
+import seventhPhoto from "../assets/mobile-7.png";
+import eighthPhoto from "../assets/mobile-8.png";
+import ninthPhoto from "../assets/mobile-9.png";
+import tenthPhoto from "../assets/mobile-10.png";
+import eleventhPhoto from "../assets/mobile-11.png";
+import twelfthPhoto from "../assets/mobile-12.png";
+import thirteenthPhoto from "../assets/mobile-13.png";
+import fourteenthPhoto from "../assets/mobile-14.png";
+import fifteenthPhoto from "../assets/mobile-15.png";
+import sixteenthPhoto from "../assets/mobile-16.png";
+import seventeenthPhoto from "../assets/mobile-17.png";
+import eighteenthPhoto from "../assets/mobile-18.png";
+import nineteenthPhoto from "../assets/mobile-19.png";
+import twentiethPhoto from "../assets/mobile-20.png";
+
 
 import ReferenceAccordion from "../components/layout/Accordion";
 
@@ -50,8 +52,8 @@ export default function SmartPhone() {
 
     return (
         <section className="page-format page-gutters">
-            <Title text="【PC】基本操作" />
-            <SubTitle text="【PC】基本操作" />
+            <Title text="【iPhone】基本操作" />
+            <SubTitle text="【iPhone】基本操作" />
             <HashLinkList items={items} />
             <SubTitle idName="画面の見方" text="画面の見方" />
                 <p>
@@ -75,8 +77,8 @@ export default function SmartPhone() {
             <p>
             「素材」のアイコンをクリックし検索窓より「ハート」などご希望のイメージを検索して頂くと、色々な素材が表示されますので気に入ったものを追加して下さい
             </p>
-            <SubTitle idName="3.テキスト" text="3.テキスト" />
             <ContentImage src={secondPhoto} alt="First photo of instructions for text" />
+            <SubTitle idName="3.テキスト" text="3.テキスト" />
             <p>
             文字を新たに追加したい時は、「テキスト」のアイコンをクリックし「見出しを追加」で新たに文字を追加出来ます
             </p>
@@ -127,13 +129,11 @@ export default function SmartPhone() {
             <p>
             クリックすると曲が表示されます
             </p>
-            <ContentImage src={eighthPhoto} alt="Instructions for music part 1" />
-            <ContentImage src={ninthPhoto} alt="Instructions for music part 2" />
             <SubTitle idName="・音楽の削除の仕方" text="・音楽の削除の仕方" />
+            <ContentImage src={eighthPhoto} alt="Instructions for music part 1" />
             <p>
             紫のバーをクリック後、「1を削除」をクリックします
             </p>
-            <ContentImage src={tenthPhoto} alt="Instructions for music part 3" />
             <p>
             音楽は別で再生する場合などに使えます
             </p>
@@ -147,9 +147,11 @@ export default function SmartPhone() {
             <p>
             Step 1.紫のバーをクリックします
             </p>
+            <ContentImage src={ninthPhoto} alt="Instructions for music part 2" />
             <p>
             Step 2.左下の置き換えのアイコンをクリックします
             </p>
+            <ContentImage src={tenthPhoto} alt="Instructions for music part 3" />
             <p>
             Step 3.ご自身のアップロード欄が開くので入れ替えたい音楽を選択して下さい
             </p>
@@ -175,14 +177,18 @@ export default function SmartPhone() {
             <p>
             Step.1 ページの右上にある…ボタンをクリックします
             </p>
-            <p>
-            Step.2 「（1）を複製」をクリックします
-            </p>
             <ContentImage src={thirteenthPhoto} alt="Page instructions part 1" />
             <p>
-            ※Step.2 で「ページを追加」をクリックすると空白のページを追加することが出来ます
+            Step.2 「すべてのページを表示」を選択します
+            </p>
+            <p>
+            Step.3 「（1）を複製」をクリックします
             </p>
             <ContentImage src={fourteenthPhoto} alt="Page instructions part 2" />
+            <p>
+            ※Step.3で「ページを追加」をクリックすると空白のページを追加することが出来ます
+            </p>
+            <ContentImage src={fifteenthPhoto} alt="Page instructions" />
             <SubTitle idName="5.アニメーション" text="5.アニメーション" />
             <p>
             文字や写真の出方を変更することが出来ます
@@ -193,14 +199,14 @@ export default function SmartPhone() {
             <p>
             Step.2 丸3つが重なっているアイコンの「アニメート」をクリックします
             </p>
-            <ContentImage src={fifteenthPhoto} alt="Animation instructions" />
+            <ContentImage src={sixteenthPhoto} alt="Animation instructions part 2" />
             <p>
             Step.3 好みのアニメーションを選択します
             </p>
             <p>
             ※既にアニメーションが追加されている場合は、「アニメート」でなくアニメーションの名前（フェード・ブリーズなど）が記載されています
             </p>
-            <ContentImage src={sixteenthPhoto} alt="Animation instructions part 2" />
+            <ContentImage src={seventeenthPhoto} alt="Final instructions part 1" />
             <SubTitle idName="6.透明度" text="6.透明度" />
             <p>
             写真の透け具合を変更することが出来ます
@@ -208,11 +214,11 @@ export default function SmartPhone() {
             <p>
             Step.1 透明度を変更したい写真をクリックします
             </p>
+            <ContentImage src={eighteenthPhoto} alt="Final instructions part 2" />
             <p>
             Step.2 「透明度」のアイコンをクリックすると、透明度を調節することが出来ます
             </p>
-            <ContentImage src={seventeenthPhoto} alt="Final instructions part 1" />
-            <ContentImage src={eighteenthPhoto} alt="Final instructions part 2" />
+            <ContentImage src={nineteenthPhoto} alt="Lock instructions" />
             <SubTitle idName="7.ロック" text="7.ロック" />
             <p>
             作業をしやすくするため、こちらで素材をロックしていることがございます
@@ -223,7 +229,7 @@ export default function SmartPhone() {
             <p>
             ご自身で作業している時も動いて欲しくない素材をロックすることで、作業しやすくなります
             </p>
-            <ContentImage src={nineteenthPhoto} alt="Lock instructions" />
+            <ContentImage src={twentiethPhoto} alt="Lock instructions" />
             <div className="py-20">
                 <ReferenceAccordion />
             </div>
