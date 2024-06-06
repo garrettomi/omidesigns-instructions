20be67244f1dbde9338d0ca9a754aea8ed9b8d7c
garrettomi <omigarrett@gmail.com>
2024-06-06 19:37:16 +0900

    Fix: Fix Links on Setsumei page

--- src/pages/Setsumei.jsx
+++ src/pages/Setsumei.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 
 import Title from "../components/common/Title";
-import SubTitle from "../components/common/SubTitle";
 import ReferenceAccordion from '../components/layout/Accordion';
 import ScrollToTop from "../components/common/ScrollToTop";
 
@@ -9,19 +8,28 @@ export default function Setsumei() {
     return (
         <section className="page-gutters page-format">
             <Title text="説明動画" />
-            <SubTitle text="説明動画" />
                 <p>
-                Youtubeで作成方法を解説しています
+                    Youtubeで作成方法を解説しています
                 </p>
                 <p>
-                以下からご確認ください
+                    以下からご確認ください
                 </p>
-                <Link to="https://www.youtube.com/watch?si=J8s1OzPUGi18u49U&v=4wDsZEWrc5w&feature=youtu.be">
-                PC用
+                <div>
+                    <Link 
+                        to="https://www.youtube.com/watch?si=J8s1OzPUGi18u49U&v=4wDsZEWrc5w&feature=youtu.be"
+                        className="font-bold hover:underline"
+                    >
+                        PC用
+                    </Link>
+                </div>
+                <div>
+                    <Link 
+                        to="https://www.youtube.com/watch?si=TW29WzjclEcw1GLX&v=8xtys9QUsP0&feature=youtu.be"
+                        className="font-bold hover:underline"
+                    >
+                        iPhone用
                 </Link>
-                <Link to="https://www.youtube.com/watch?si=TW29WzjclEcw1GLX&v=8xtys9QUsP0&feature=youtu.be">
-                iPhone用
-                </Link>
+                </div>
                 <div className="py-20">
                     <ReferenceAccordion />
                 </div>
