b7fa5afb21419bae18bc58d66e586e54b46c1f77
garrettomi <omigarrett@gmail.com>
2024-06-06 20:28:34 +0900

    Fix: Remove Contact from Navbar

--- src/components/layout/Navbar/components/DesktopNavItems.jsx
+++ src/components/layout/Navbar/components/DesktopNavItems.jsx
@@ -10,10 +10,13 @@ export default function DesktopNavItems() {
             <ul className="lg:flex flex-col lg:flex-row items-center gap-8">
                 <DesktopNavItem text="Home" url="/" />
                 <DesktopNavItem text="About" url="/about" />
-                <DesktopNavItem text="Shop" url="/shop" />
-                <DesktopNavItem text="Contact" url="/contact" />                             
+                <DesktopNavItem text="Shop" url="https://shop.omidesigns.net/" />                   
                 <li>
-                    <a href="https://instagram.com/omidesigns_weddingtemplate" target="_blank" rel="noopener noreferrer" className="text-omidesigns-brown hover:text-gray-600 transition">
+                    <a href="https://instagram.com/omidesigns_weddingtemplate" 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="text-omidesigns-brown hover:text-gray-600 transition"
+                    >
                         <InstagramIcon color="#8B806C" />
                     </a>
                 </li>
--- src/components/layout/Navbar/components/MobileNavItems.jsx
+++ src/components/layout/Navbar/components/MobileNavItems.jsx
@@ -9,10 +9,16 @@ export default function MobileNavItems({ isOpen }) {
         <div className="lg:hidden bg-[#EBE3DB]">
             <MobileNavItem url="/" label="Home" />
             <MobileNavItem url="/about" label="About" />
-            <MobileNavItem url="/shop" label="Shop" />
-            <MobileNavItem url="/contact" label="Contact" />
+            <MobileNavItem 
+                url="https://shop.omidesigns.net/" 
+                label="Shop" 
+            />
             <div className="p-5">
-            <a href="https://instagram.com/omidesigns_weddingtemplate" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600 transition">
+            <a 
+                href="https://instagram.com/omidesigns_weddingtemplate" 
+                target="_blank" rel="noopener noreferrer" 
+                className="text-gray-800 hover:text-gray-600 transition"
+            >
                 <InstagramIcon color="black" />
             </a>
             </div>
