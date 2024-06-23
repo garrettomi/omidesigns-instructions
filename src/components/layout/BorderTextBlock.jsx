de645a25d7b1ee654c5ca60a35b7ebe4a93e7b12
garrettomi <omigarrett@gmail.com>
2024-06-23 21:20:32 +0900

    Fix: Font sizing on mobile homepage

--- src/components/layout/BorderTextBlock.jsx
+++ src/components/layout/BorderTextBlock.jsx
@@ -2,10 +2,10 @@ export default function BorderTextBlock ({ firstSentence, secondSentence }) {
     return (
         <div className="bg-[#ebe3db] w-screen h-28 mx-auto px-4 sm:px-6 md:px-8 py-2 flex flex-col items-center justify-center animate-fadeIn">
             <div>
-                <h6 className="font-bold text-sm text-center sm:text-md md:text-lg">{firstSentence}</h6>
+                <h6 className="font-bold text-xs text-center sm:text-md md:text-lg">{firstSentence}</h6>
             </div>
             <div>
-                <h6 className="font-bold text-sm text-center sm:text-md md:text-lg">{secondSentence}</h6>
+                <h6 className="font-bold text-xs text-center sm:text-md md:text-lg">{secondSentence}</h6>
             </div>
         </div>
     );
